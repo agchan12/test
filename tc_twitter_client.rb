d94246140a1b8f780ff3e2f7eac2910fe145c905
agchan12 <saru.see.saru.do@gmail.com>
2016-09-14 21:48:20 +1000

    Update tc_twitter_client.rb

--- tc_twitter_client.rb
+++ tc_twitter_client.rb
@@ -8,9 +8,9 @@ class TestTwitterClient < Test::Unit::TestCase
     hash = TwitterClient.instance.count_tweets("ABC")
     assert_equal(24, hash.size())
 
-    # Assume twitter user "Xinyu_Li"
+    # Assume twitter user "do_saru (a twitter account that I made up for the purposes of testing)"
     # post no tweets in last 24 hours
-    hash = TwitterClient.instance.count_tweets("Xinyu_Li")
+    hash = TwitterClient.instance.count_tweets("do_saru")
     assert_equal(24, hash.size())
     # test all the tweets count are 0
     assert_equal(0, hash.values.inject(0){|sum,x| sum + x })
